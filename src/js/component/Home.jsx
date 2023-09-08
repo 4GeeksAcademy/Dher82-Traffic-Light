2ee3bbbf15eec0bcfbd69439b22ac159870c6f96
Dher82 <126974687+Dher82@users.noreply.github.com>
2023-09-08 22:53:31 +0000

    Traffic Light-Dave

---
+++ src/js/component/Home.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+//create your first component
+
+export function Home() {
+    const [selectedColor, setSelectedColor] = useState("");
+    return (
+        <div className="traffic-light">
+            <div
+                onClick={() => setSelectedColor("red")}
+                className={"light red" + (selectedColor === "red" ? "glow" : " ")}></div>
+            <div
+                onClick={() => setSelectedColor("yellow")}
+                className={"light yellow" + (selectedColor === "yellow" ? "glow" : " ")}></div>
+            <div
+                onClick={() => setSelectedColor("green")}
+                className={"light green" + (selectedColor === "green" ? "glow" : " ")}></div>
+        </div>
+
+    );
+}
